67aac382e13783eefcf4b46f92d1b68a83d19c48
José Alberto Pérez Melián <japmelian@gmail.com>
2013-09-25 21:13:57 +0100

    Finalizando

---
+++ .configure.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "En2mseR4whskkZ8SmCjEQ"
+  config.consumer_secret = "FD1GBpFtbbQYJ80VbgG5a2ZfksrDg0W7AESd8eXGRg"
+  config.oauth_token = "298792445-058ViGriH3Vw2GgrB4DPmxsKEc6r6WnneuvJZTSo"
+  config.oauth_token_secret = "HjvPPMRAIKLQvPmTOna1mzheSEW2VGeFMBuCkfzxUI"
+end
