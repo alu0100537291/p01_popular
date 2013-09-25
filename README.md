a917cf3cb88f112054a44132cc80702d5c0dcc23
José Alberto Pérez Melián <japmelian@gmail.com>
2013-09-25 21:08:54 +0100

    Subiendo

---
+++ README.md
@@ -0,0 +1,19 @@
+SYTW - PRÁCTICA 01 - Popular users on Twitter
+=============================================
+
+* Alumno: José Alberto Pérez Melián
+* E-mail: alu0100537291[AT]ull.edu.es
+
+
+El objetivo de esta práctica es dado un usuario de twitter obtener un ranking de usuarios que esa persona siga según el número de personas que sigan a ese usuario.
+
+
+
+¡IMPORTANTE!
+------------
+
+Recuerde configurar el fichero *configure.rb* con:
+   * Clave y secreto del consumidor
+   * OAUTH token
+   * OAUTH secret token
+Ésto lo podrá hacer dando de alta la aplicación en http://dev.twitter.com
---
+++ popular.rb
@@ -0,0 +1,50 @@
+require 'twitter'
+require './configure'
+
+#Hash where followers are stored
+user = Hash.new
+
+#User
+screen_name = ARGV[0] || 'japmelian'
+
+#Iterations
+iter = ARGV[1] || '5'
+
+#Saving user
+a_user = Twitter.user(screen_name)
+
+#Username on screen
+puts "Username   : #{a_user.screen_name}"
+
+#Saving friends ID's
+friends = Twitter.friend_ids(screen_name)
+
+#Counter
+counter = 0 
+
+#Go over the Hash 'user', saving as many users as the app user wants
+friends.ids.each do |fid|
+	if (counter < iter.to_i)
+		f = Twitter.user(fid)
+
+      #If a user account is not protected, introduce it to Hash 'user'
+		if (f.protected.to_s != "true")  
+			user[f.screen_name.to_s] = f.followers_count
+			counter += 1
+		end
+	end
+end
+
+var = 0
+
+#Saving users of Hash 'user' sorted by number of followers
+users = user.sort_by {|k,v| -v}
+
+#User most popular friends listed and sorted
+usuarios.each do |user,count|
+	if (var > iter.to_i)
+		break
+	end
+	puts "#{user},#{count}"
+	var +=1
+end
